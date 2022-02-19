f625a31bb4926a3a6e1329541eea1470a22479df
Gabriel Salazar <32026770+cubicles@users.noreply.github.com>
2022-02-19 17:35:14 -0500

    practice

---
+++ classes.py
@@ -0,0 +1,13 @@
+# A Sample class with init method
+class Person:
+
+    # init method or constructor
+    def __init__(self, name):
+        self.name = name
+
+    # Sample method
+    def say_hi(self):
+        print('Hello my name is', self.name)
+
+p = Person('Gabriel')
+p.say_hi()
---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+# Print the n-th numbers of the fibonacci sequence
+
+n = input("Enter n \n")
+
+for i in range(1, n):
+    print i
+
+
+
